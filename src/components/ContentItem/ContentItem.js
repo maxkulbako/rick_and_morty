d09552fd5835c6dac139a8ef67d07d4f4426278b
max.kulbako <mkulbako@gmail.com>
2023-03-08 18:20:07 +0100

    add lazy loading of images

--- src/components/ContentItem/ContentItem.js
+++ src/components/ContentItem/ContentItem.js
@@ -1,10 +1,21 @@
+import { useInView } from 'react-intersection-observer';
+
 import './contentItem.scss';
 
 export function ContentItem({ item }) {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    triggerOnce: true
+  });
+
   if (item) {
     return (
-      <div className="item_wrapper">
-        <img src={item.image} alt={item.name} />
+      <div ref={ref} className="item_wrapper">
+        {inView ? (
+          <img src={item.image} alt={item.name} />
+        ) : (
+          <div className="item_wrapper_skeleton"></div>
+        )}
         <p>{item.name}</p>
         <p>{item.species}</p>
       </div>
